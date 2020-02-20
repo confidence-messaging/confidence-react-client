456effbaaf0e1b833e18ead5cce7870774def642
Gustavo <gugurock50@gmail.com>
2020-02-19 21:51:42 -0300

    added message component

---
+++ src/components/Message.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import { } from 'reactstrap';
+
+export default class Modal extends Component{
+    constructor(){
+
+    }
+
+    render(){
+        return 
+    }
+}
